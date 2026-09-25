99d8ec59cff72955dba734d836af7419e549f90d
Era Corp <zeetzy.id@gmail.com>
2026-09-26 00:43:25 +0700

    feat: use otakuproxy.zerowebsite.eu.org as default upstream proxy

--- helpers/config.js
+++ helpers/config.js
@@ -1,4 +1,4 @@
-const BASE_URL = (process.env.OTAKUDESU_BASE_URL || "https://otakudesu.xyz/").replace(/\/?$/, "/");
+const BASE_URL = (process.env.OTAKUDESU_BASE_URL || "https://otakuproxy.zerowebsite.eu.org/").replace(/\/?$/, "/");
 const MIRRORS = (process.env.OTAKUDESU_MIRRORS || "")
   .split(",")
   .map((s) => s.trim().replace(/\/?$/, "/"))
